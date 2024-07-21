bfbf1840b1c0b20b37a528e74a574ecbcebcfca8
Ajit J <ajunghare123@gmail.com>
2024-07-22 01:34:28 +0530

    Story 3: read csv from cloud storage and load into Bigquery table

--- cloud_functions/hello-world-1/main.py
+++ cloud_functions/hello-world-1/main.py
@@ -1,26 +1,45 @@
-import functions_framework.background_event
+from google.cloud import bigquery
 
-
-# Register a CloudEvent function with the Functions Framework
-# @functions_framework.background_event
 def invoke_on_event(event_data, context):
-    # Your code here
-    print ("--------#######---------")
-    print(f"Event ID: {context.event_id}")
-    print(f"Event type: {context.event_type}")
-    print("Bucket: {}".format(event_data["bucket"]))
-    print("File: {}".format(event_data["name"]))
-    print("Metageneration: {}".format(event_data["metageneration"]))
-    print("Created: {}".format(event_data["timeCreated"]))
-    print("Updated: {}".format(event_data["updated"]))
-    print ("--------#######---------")
+    print("--------#######---------")
     filename = event_data["name"]
+    bucket_name = event_data["bucket"]
     if filename.startswith("movies_") and filename.endswith(".csv"):
-        #this file could be added to GBQ
-        from google.cloud import bigquery as bq
-        client = bq.Client()
-        dataset_id = "ee-india-se-data.movies_data_ajit"
-        dataset = bq.Dataset(dataset_id)
-        dataset = client.get_dataset(dataset)
 
-        
+        client = bigquery.Client()
+        project = client.project
+        dataset_id = "movies_data_ajit"
+        dataset_ref = bigquery.DatasetReference(project, dataset_id)
+
+        table_id = "movies_raw"
+        table_ref = dataset_ref.table(table_id)
+        table = client.get_table(table_ref)
+        print("Table {} contains {} columns.".format(table_id, len(table.schema)))
+
+        job_config = bigquery.LoadJobConfig()
+        job_config.write_disposition = bigquery.WriteDisposition.WRITE_APPEND
+        job_config.schema_update_options = [
+            bigquery.SchemaUpdateOption.ALLOW_FIELD_ADDITION
+        ]
+        schema_list = []
+        print("table schema list", table.schema)
+        for field in table.schema:
+            if field.name != "load_date":
+                schema_list.append(bigquery.SchemaField(name=field.name, field_type=field.field_type, mode=field.mode))
+
+        job_config.schema = schema_list
+        job_config.source_format = bigquery.SourceFormat.CSV
+        job_config.skip_leading_rows = 1
+        job = client.load_table_from_uri(
+            "gs://{bucket_name}/{filename}".format(bucket_name=bucket_name,filename=filename),
+            table_ref,
+            location="asia-south1",  # Must match the destination dataset location.
+            project=project,
+            job_config=job_config,
+        )  # API request
+        job.result()  # Waits for table load to complete.
+        print(
+            "Loaded {} rows into {}:{}.".format(
+                job.output_rows, dataset_id, table_ref.table_id
+            )
+        )
