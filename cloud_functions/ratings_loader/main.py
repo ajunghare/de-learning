787d3282bb7a532bc1515edf6ec40680dd936956
Ajit J <ajunghare123@gmail.com>
2024-07-23 01:25:37 +0530

    Story 5: ratings data cloud function

---
+++ cloud_functions/ratings_loader/main.py
@@ -0,0 +1,36 @@
+from google.cloud import bigquery
+
+def invoke_on_event(event_data, context):
+    print("--------#######---------")
+    filename = event_data["name"]
+    bucket_name = event_data["bucket"]
+    if filename.startswith("ratings_") and filename.endswith(".csv"):
+
+        client = bigquery.Client()
+        project = client.project
+        dataset_id = "movies_data_ajit"
+        dataset_ref = bigquery.DatasetReference(project, dataset_id)
+
+        table_id = "ratings"
+        table_ref = dataset_ref.table(table_id)
+        table = client.get_table(table_ref)
+        print("Table {} contains {} columns.".format(table_id, len(table.schema)))
+
+        job_config = bigquery.LoadJobConfig()
+        job_config.write_disposition = bigquery.WriteDisposition.WRITE_APPEND
+        
+        job_config.source_format = bigquery.SourceFormat.CSV
+        job_config.skip_leading_rows = 1
+        job = client.load_table_from_uri(
+            "gs://{bucket_name}/{filename}".format(bucket_name=bucket_name,filename=filename),
+            table_ref,
+            location="asia-south1",  # Must match the destination dataset location.
+            project=project,
+            job_config=job_config,
+        )  # API request
+        job.result()  # Waits for table load to complete.
+        print(
+            "Loaded {} rows into {}:{}.".format(
+                job.output_rows, dataset_id, table_ref.table_id
+            )
+        )
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
-
 resource "google_storage_bucket" "landing-bucket" {
   name                     = "se-data-landing-ajit"
   location                 = "ASIA-SOUTH1"
   force_destroy            = true
   public_access_prevention = "enforced"
+
   lifecycle_rule {
     condition {
       age = 1
@@ -26,163 +26,15 @@ resource "google_bigquery_dataset" "movies_data" {
   }
 }
 
-resource "google_bigquery_table" "movies_raw" {
-  dataset_id          = google_bigquery_dataset.movies_data.dataset_id
-  table_id            = "movies_raw"
-  deletion_protection = false
-  schema              = <<EOF
-    [
-      {
-        "name": "adult",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "adult"
-      },
-      {
-        "name": "belongs_to_collection",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "belongs_to_collection"
-      },
-      {
-        "name": "budget",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "budget"
-      },
-      {
-        "name": "genres",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "genres"
-      },
-      {
-        "name": "homepage",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "homepage"
-      },
-      {
-        "name": "id",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "id"
-      },
-      {
-        "name": "imdb_id",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "imdb_id"
-      },
-      {
-        "name": "original_language",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "original_language"
-      },
-      {
-        "name": "original_title",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "original_title"
-      },
-      {
-        "name": "overview",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "overview"
-      },
-      {
-        "name": "popularity",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "popularity"
-      },
-      {
-        "name": "poster_path",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "poster_path"
-      },
-      {
-        "name": "production_companies",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "production_companies"
-      },
-      {
-        "name": "production_countries",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "production_countries"
-      },
-      {
-        "name": "release_date",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "release_date"
-      },
-      {
-        "name": "revenue",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "revenue"
-      },
-      {
-        "name": "runtime",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "runtime"
-      },
-      {
-        "name": "spoken_languages",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "spoken_languages"
-      },
-      {
-        "name": "status",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "status"
-      },
-      {
-        "name": "tagline",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "tagline"
-      },
-      {
-        "name": "title",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "title"
-      },
-      {
-        "name": "video",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "video"
-      },
-      {
-        "name": "vote_average",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "vote_average"
-      },
-      {
-        "name": "vote_count",
-        "type": "STRING",
-        "mode": "NULLABLE",
-        "description": "vote_count"
-      },
-      {
-        "name": "load_date",
-        "type": "DATETIME",
-        "defaultValueExpression": "CURRENT_DATETIME()",
-        "mode": "NULLABLE",
-        "description": "vote_count"
-      }
-    ] 
-    EOF
+resource "google_cloudfunctions_function" "ratings_loader" {
+  name = "ratings_loader"
+  runtime = "python312"
+  source_repository {
+    url = "https://github.com/ajunghare/se-data-eng-excercise/tree/main/cloud_functions/ratings_loader"
+  }
+  event_trigger {
+    resource = google_storage_bucket.landing-bucket.name
+    event_type = "google.storage.object.finalize"
+  }
+  
 }
