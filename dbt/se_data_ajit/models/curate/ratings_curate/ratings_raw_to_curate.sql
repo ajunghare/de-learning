9f65e92421b645da0c104e643a8a5c6220354041
Ajit J <ajunghare123@gmail.com>
2024-07-28 21:37:32 +0530

    Story 9: Remove duplicates from ratings_curate

--- dbt/se_data_ajit/models/curate/ratings_curate/ratings_raw_to_curate.sql
+++ dbt/se_data_ajit/models/curate/ratings_curate/ratings_raw_to_curate.sql
@@ -1,20 +1,39 @@
 {{config(
-    materialized='incremental', alias='ratings_curate')}}
+    materialized='incremental', 
+    alias='ratings_curate',
+    unique_key=['userId','movieId'])}}
 
-
+with latest_rating as(
 select
     cast (userId as INT) as userId,
     cast (movieId as INT) as movieId,
     cast (rating as DECIMAL) as rating,
-    timestamp,
+    cast (timestamp as INT) as timestamp,
     load_time
 from 
-{{ source('raw','ratings_raw') }}
-{% if is_incremental() %}
-  -- (uses >= to include records whose timestamp occurred since the last run of this model)
-  -- (If event_time is NULL or the table is truncated, the condition will always be true and load all records)
-where load_time >= (select coalesce(max(load_time),'1900-01-01') from {{ this }} )
-
-{% endif %}
-
-
+  {{ source('raw','ratings_raw') }}
+  {% if is_incremental() %}
+  where load_time >= (select coalesce(max(load_time),'1900-01-01') from {{ this }} )
+  {% endif %}
+),
+max_timestamp_per_user_per_movie as (
+select
+    userId,
+    movieId,
+    max(timestamp) ts
+    from latest_rating
+    group by userId, movieId
+    )
+select 
+    lr.userId
+  , lr.movieId
+  , lr.timestamp
+  , lr.rating
+  , lr.load_time 
+  from 
+max_timestamp_per_user_per_movie mt 
+  inner join 
+latest_rating lr 
+  on  lr.userId = mt.userId 
+  and lr.movieId = mt.movieId 
+  and lr.timestamp = mt.ts
